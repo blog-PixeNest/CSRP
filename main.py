1d8880fb2d57390e21d15aab2752c4bf120483a5
zhayne12 <156608502+zhayne12@users.noreply.github.com>
2024-04-08 21:58:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ keep_alive()
 import random
 # Define your developer IDs here
 developer_ids = ['719648115639975946', '719648115639975946', '719648115639975946']
-
+developer_role_id = 1222641922049445920
 token = os.environ['token']
 
 intents = discord.Intents.default()
@@ -28,6 +28,26 @@ async def on_ready():
 async def ping(ctx):
     latency = bot.latency
     await ctx.send(f'Pong! Latency: **{round(latency * 1000)}**ms')
+
+@bot.command(name='announce', help='Announce a message to developers only')
+async def announce(ctx, *, message):
+    """Announce a message to developers only."""
+    if any(role.id == developer_role_id for role in ctx.author.roles):
+        developer_role = discord.utils.get(ctx.guild.roles, id=developer_role_id)
+        await developer_role.send(f"📢 **Developer Announcement:** {message}")
+        await ctx.send("Announcement sent to developers.")
+    else:
+        await ctx.send("You do not have permission to use this command.")
+
+
+
+
+
+
+
+
+
+
 
 bot.run(token)
   
