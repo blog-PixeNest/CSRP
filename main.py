17af48e94574926fcf3a5aa5416ddb13acaf4ac9
zhayne12 <156608502+zhayne12@users.noreply.github.com>
2024-04-08 22:05:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ keep_alive()
 import random
 # Define your developer IDs here
 developer_ids = ['719648115639975946', '719648115639975946', '719648115639975946']
-developer_role_id = 1222641922049445920
+
 token = os.environ['token']
 
 intents = discord.Intents.default()
@@ -29,17 +29,20 @@ async def ping(ctx):
     latency = bot.latency
     await ctx.send(f'Pong! Latency: **{round(latency * 1000)}**ms')
 
+developer_role_id = 1222641922049445920  # Replace with your developer role ID
+announcement_channel_name = "announcement"  # Replace with the name of the announcement channel
+
 @bot.command(name='announce', help='Announce a message to developers only')
 async def announce(ctx, *, message):
     """Announce a message to developers only."""
     if any(role.id == developer_role_id for role in ctx.author.roles):
-        developer_role = discord.utils.get(ctx.guild.roles, id=developer_role_id)
-        await developer_role.send(f"📢 **Developer Announcement:** {message}")
-        await ctx.send("Announcement sent to developers.")
+        for guild in bot.guilds:
+            announcement_channel = discord.utils.get(guild.channels, name=announcement_channel_name, type=discord.ChannelType.text)
+            if announcement_channel:
+                await announcement_channel.send(f"📢 **Developer Announcement:** {message}")
+        await ctx.send("Announcement sent to all servers.")
     else:
         await ctx.send("You do not have permission to use this command.")
-
-
 
 
 
