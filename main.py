6828e59e1481363b5ddb09fa33975c590e29a3c3
zhayne12 <156608502+zhayne12@users.noreply.github.com>
2024-04-09 21:47:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ keep_alive()
 import random
 import asyncio
 import sqlite3
+from discord_slash import SlashCommand
+
 
 # Define your developer IDs here
 developer_ids = ['719648115639975946', '719648115639975946', '719648115639975946']
@@ -18,34 +20,24 @@ intents.reactions = True
 intents.guilds = True
 intents.members = True
 
-
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.default())
+slash = SlashCommand(bot, sync_commands=True)  # Declares slash commands through the bot.
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
     await bot.change_presence(activity=discord.Game(name="in development"))
+
+@slash.slash(name="hello", description="Says hello!")
+async def _hello(ctx):
+    await ctx.send("Hello!")
+
+@slash.slash(name="roll", description="Rolls a dice")
+async def _roll(ctx, sides: int):
+    import random
+    result = random.randint(1, sides)
+    await ctx.send(f'You rolled a {result} (1-{sides}).')
   
-#ping command
-@bot.command(name='ping')
-async def ping(ctx):
-    latency = bot.latency
-    await ctx.send(f'Pong! Latency: **{round(latency * 1000)}**ms')
-
-developer_role_id = 1222641922049445920  # Replace with your developer role ID
-announcement_channel_name = "announcement"  # Replace with the name of the announcement channel
-
-@bot.command(name='announce', help='Announce a message to developers only')
-async def announce(ctx, *, message):
-    """Announce a message to developers only."""
-    if any(role.id == developer_role_id for role in ctx.author.roles):
-        for guild in bot.guilds:
-            announcement_channel = discord.utils.get(guild.channels, name=announcement_channel_name, type=discord.ChannelType.text)
-            if announcement_channel:
-                await announcement_channel.send(f"📢 **Developer Announcement:** {message}")
-        await ctx.send("Announcement sent to all servers.")
-    else:
-        await ctx.send("You do not have permission to use this command.")
 
 
 
