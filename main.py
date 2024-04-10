cc99fac413860cbdf55db9e4a8aafc4cd44a2811
zhayne12 <156608502+zhayne12@users.noreply.github.com>
2024-04-09 22:35:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ keep_alive()
 import random
 import asyncio
 import sqlite3
-from discord_slash import SlashCommand
+
 
 
 # Define your developer IDs here
@@ -20,24 +20,14 @@ intents.reactions = True
 intents.guilds = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='/', intents=discord.Intents.default())
-slash = SlashCommand(bot, sync_commands=True)  # Declares slash commands through the bot.
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
     await bot.change_presence(activity=discord.Game(name="in development"))
 
-@slash.slash(name="hello", description="Says hello!")
-async def _hello(ctx):
-    await ctx.send("Hello!")
 
-@slash.slash(name="roll", description="Rolls a dice")
-async def _roll(ctx, sides: int):
-    import random
-    result = random.randint(1, sides)
-    await ctx.send(f'You rolled a {result} (1-{sides}).')
-  
 
 
 
